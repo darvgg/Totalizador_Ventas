e5c197928f60e4a32753da624e5c8b5b892898cb
MarceloNaviaR <marcelo.navia@gmail.com>
2025-03-02 20:47:11 -0400

    Modificacion en presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,11 @@
-import { mostrar, precio_neto } from "./totalizador";
+import { mostrar } from "./totalizador.js";
 
 const cant_item = document.querySelector("#primer-numero");
 const precio_item = document.querySelector("#segundo-numero");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
-const estado_select = document.querySelector("#estados-Select")
+const estado_select = document.querySelector("#estados-Select");
+const categoria_select = document.querySelector("#categoria-Select"); // Nuevo select de categoría
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,6 +13,7 @@ form.addEventListener("submit", (event) => {
   const cantItem = Number.parseFloat(cant_item.value);
   const precioItem = Number.parseFloat(precio_item.value);
   const cod_estado = estado_select.value;
+  const categoria = categoria_select.value; // Obtener la categoría seleccionada
 
-  div.innerHTML = "<p>" + mostrar(cantItem, precioItem,cod_estado) + "</p>";
+  div.innerHTML = "<p>" + mostrar(cantItem, precioItem, cod_estado, categoria) + "</p>";
 });
